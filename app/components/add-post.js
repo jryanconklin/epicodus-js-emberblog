77733e1b162da5adfbf693f9bded6db51a2d1d12
Katrina Hirsch and J. Ryan Conklin <katrina@>
2016-10-26 16:05:04 -0700

    add functionality to create new post

--- app/components/add-post.js
+++ app/components/add-post.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    savePost() {
+      var params = {
+        title: this.get('title'),
+        author: this.get('author'),
+        body: this.get('body'),
+        image: this.get('image')
+      };
+      this.sendAction('savePost', params);
+    }
+  }
 });
